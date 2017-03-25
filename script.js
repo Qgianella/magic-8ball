ee21988e30fd35f585aa99aca67805ac78b05e22
QGianella <gianella7@gmail.com>
2017-03-24 21:02:52 -0700

    comenzando 8ball

---
+++ script.js
@@ -0,0 +1,23 @@
+var magicBall = {};
+magicBall.answer = ["Outlook good", "Yes", "try again", "Better not tell you now", "Cannot predict now", "Concentrate and ask again", "Don't count on it", "My reply is no", "My sources say no"];
+var question =prompt ("Ask me anything and I shall respond..");
+
+//define the method
+magicBall.askQuestion = function(question) {
+
+  //create a random number
+  var randomNumber = Math.random();
+
+  //Make random Number between 0 and the nomber of items in array
+  var randomNumberArray = randomNumber * this.answer.length;
+
+  //Round down the number to be a whole Number
+  var randomIndex = Math.floor(randomNumberArray);
+
+  //Use that number to index a random number from the array
+  var randomAnswer = this.answer[randomIndex];
+  console.log(question);
+  console.log(randomAnswer);
+};
+
+magicBall.askQuestion(question);
